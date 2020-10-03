27b064a426575a8cc0396d0a17a9d7f98ee57ca7
Adrian Majewski <andrevfcb@gmail.com>
2020-10-03 12:22:54 +0200

    Updates

---
+++ precache-manifest.205270ba485d84dd9aa4712513bdcd56.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d65707a0aeec6d6e3cf22b200e0fe0a",
+    "url": "/React_Router_FCB/index.html"
+  },
+  {
+    "revision": "729955f1014500e60ba8",
+    "url": "/React_Router_FCB/static/css/main.ff8107e9.chunk.css"
+  },
+  {
+    "revision": "02dac695375e1738d805",
+    "url": "/React_Router_FCB/static/js/2.6b27b209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Router_FCB/static/js/2.6b27b209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729955f1014500e60ba8",
+    "url": "/React_Router_FCB/static/js/main.b7671bd4.chunk.js"
+  },
+  {
+    "revision": "1ceedebf06e198fabd63",
+    "url": "/React_Router_FCB/static/js/runtime-main.344e967e.js"
+  },
+  {
+    "revision": "28aae8288c58269b8a3cc4ffa56042f6",
+    "url": "/React_Router_FCB/static/media/Ansu_Fati.28aae828.jpg"
+  },
+  {
+    "revision": "b579c0aefd34902ec315371bed68dee5",
+    "url": "/React_Router_FCB/static/media/Antoine_Griezmann.b579c0ae.jpg"
+  },
+  {
+    "revision": "7e0d244f253e4813fe9378a132f87627",
+    "url": "/React_Router_FCB/static/media/Carles_Alena.7e0d244f.jpg"
+  },
+  {
+    "revision": "66a6661928ab76eefa27b75545d53470",
+    "url": "/React_Router_FCB/static/media/Champions-League.66a66619.png"
+  },
+  {
+    "revision": "0341296bd83c7cddc4011d04a8fbb105",
+    "url": "/React_Router_FCB/static/media/Clement_Lenglet.0341296b.jpg"
+  },
+  {
+    "revision": "8d8f8ca921055ec39cdc28d9739bf685",
+    "url": "/React_Router_FCB/static/media/Coutinho.8d8f8ca9.jpg"
+  },
+  {
+    "revision": "b3e3835fa2e72f36abd1c2fd6f52b652",
+    "url": "/React_Router_FCB/static/media/European-Super-Cup.b3e3835f.png"
+  },
+  {
+    "revision": "605bff822e047d11be1390e7a1f3e3e5",
+    "url": "/React_Router_FCB/static/media/Fifa-Club-World-Cup.605bff82.png"
+  },
+  {
+    "revision": "2cc0c913d0cda10d11983ca476ea8bb5",
+    "url": "/React_Router_FCB/static/media/Francisco_Trincao.2cc0c913.jpeg"
+  },
+  {
+    "revision": "c80507108f3805621dd38c81520ccda2",
+    "url": "/React_Router_FCB/static/media/Frenkie_De_Jong.c8050710.jpg"
+  },
+  {
+    "revision": "e9c2a5cfbb33ef31047df9b08e71c482",
+    "url": "/React_Router_FCB/static/media/Gerard_Pique.e9c2a5cf.jpg"
+  },
+  {
+    "revision": "6ac4d0187d56a9baadf2e5838318634e",
+    "url": "/React_Router_FCB/static/media/Inaki_Pena.6ac4d018.jpg"
+  },
+  {
+    "revision": "b38ea749a19517d32c9645bff8fc3b0c",
+    "url": "/React_Router_FCB/static/media/Jean_Clair_Todibo.b38ea749.jpg"
+  },
+  {
+    "revision": "692077255a836537b48556e6a3dbb7e7",
+    "url": "/React_Router_FCB/static/media/Jordi_Alba.69207725.jpg"
+  },
+  {
+    "revision": "e615b05e5797f5665f4a895c8a3f11b7",
+    "url": "/React_Router_FCB/static/media/Juan_Miranda.e615b05e.jpg"
+  },
+  {
+    "revision": "319d9a4b418dbbb826f6539821f5f73c",
+    "url": "/React_Router_FCB/static/media/Junior_Firpo.319d9a4b.jpg"
+  },
+  {
+    "revision": "9a3cfcfdedffde79c67190ced6bc3cc1",
+    "url": "/React_Router_FCB/static/media/Lionel_Messi.9a3cfcfd.jpg"
+  },
+  {
+    "revision": "45035a78b610d41f95e931a5e25619d2",
+    "url": "/React_Router_FCB/static/media/Marc_Andre_Ter_Stegen.45035a78.jpg"
+  },
+  {
+    "revision": "beebc531813280f099971180dd1e90ba",
+    "url": "/React_Router_FCB/static/media/Martin_Braithwaite.beebc531.jpg"
+  },
+  {
+    "revision": "24a0c3f4d11364470fab485058f4610b",
+    "url": "/React_Router_FCB/static/media/Miralem_Pjanic.24a0c3f4.jpg"
+  },
+  {
+    "revision": "e1cb3e4162aef6d02e209e4f86957632",
+    "url": "/React_Router_FCB/static/media/Moussa_Wague.e1cb3e41.jpg"
+  },
+  {
+    "revision": "75d1fa3e12c2b7633f67997cbf08ca63",
+    "url": "/React_Router_FCB/static/media/Neto.75d1fa3e.jpg"
+  },
+  {
+    "revision": "b48dd7eb0bde6dd73ed06cda1f5d1025",
+    "url": "/React_Router_FCB/static/media/Ousmanne_Dembele.b48dd7eb.jpg"
+  },
+  {
+    "revision": "a198a97f613c2ba2231f64f27865b5ef",
+    "url": "/React_Router_FCB/static/media/Pedri.a198a97f.jpg"
+  },
+  {
+    "revision": "52a341cac6f9abf7856951c917f889a4",
+    "url": "/React_Router_FCB/static/media/Rafinha.52a341ca.jpg"
+  },
+  {
+    "revision": "4852aa20ee89661ef0dbd5f6c25f47df",
+    "url": "/React_Router_FCB/static/media/Riqui_Puig.4852aa20.jpg"
+  },
+  {
+    "revision": "a8c94801ebe949ee14f1227c21a454b4",
+    "url": "/React_Router_FCB/static/media/Ronald_Araujo.a8c94801.jpg"
+  },
+  {
+    "revision": "56aec55a1f153111d331196036eb895b",
+    "url": "/React_Router_FCB/static/media/Samuel_Umtiti.56aec55a.jpeg"
+  },
+  {
+    "revision": "cc9cb49d80d6ff412d6df983f05d8764",
+    "url": "/React_Router_FCB/static/media/Sergi_Roberto.cc9cb49d.jpg"
+  },
+  {
+    "revision": "72777361412c8395e70608578f10d52e",
+    "url": "/React_Router_FCB/static/media/Sergio_Busquets.72777361.jpeg"
+  },
+  {
+    "revision": "f44db9dd791f0e2a804b4b6c12b62f3f",
+    "url": "/React_Router_FCB/static/media/Spanish-Cup.f44db9dd.png"
+  },
+  {
+    "revision": "0b90fc665e502ce0b48b1c73a32b9237",
+    "url": "/React_Router_FCB/static/media/header1.0b90fc66.jpg"
+  },
+  {
+    "revision": "42f734e0e17e9c809e6fc6463addfe48",
+    "url": "/React_Router_FCB/static/media/header2.42f734e0.jpg"
+  },
+  {
+    "revision": "6450a46bf972dbed31398167443732f2",
+    "url": "/React_Router_FCB/static/media/header3.6450a46b.jpg"
+  },
+  {
+    "revision": "0b5d6a3efcefde9b5067aa17b0a02376",
+    "url": "/React_Router_FCB/static/media/header4.0b5d6a3e.jpg"
+  },
+  {
+    "revision": "bbda828eb05d05fec593764ff2da09ee",
+    "url": "/React_Router_FCB/static/media/header5.bbda828e.jpg"
+  },
+  {
+    "revision": "e5ebc92448e1b7357978b34d305daa8f",
+    "url": "/React_Router_FCB/static/media/header6.e5ebc924.jpg"
+  },
+  {
+    "revision": "7c227a6ad08e33e4978757d4c33bf780",
+    "url": "/React_Router_FCB/static/media/la-liga.7c227a6a.png"
+  }
+]);
